26edbba7da583bf74a528ab76abe88a8c359e0e1
Colin Snow <colinmsnow@gmail.com>
2020-02-02 21:43:47 -0500

    Error messages incorporated but EOF not reading

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -11,13 +11,13 @@ Adapted from ThinkOs
 #include <stdlib.h>
 
 #define LENGTH 5
-#define MAXLENGTH 30
+#define MAXLENGTH 22 // More than big enough for any integer
 
 
 
 
 int getSum(int numbers[], int n);
-char* getInput();
+char* getInput(char instring[]);
 
 int main(){
 
@@ -25,18 +25,24 @@ int main(){
     char instring[MAXLENGTH];
 
     for (int i=0; i<LENGTH; i++){
-        if (fgets(instring, MAXLENGTH, stdin) == NULL){
-            printf("%d\n", getSum(numbers, i));
-            break;
+        if (getInput(instring) == NULL){
+            printf("Sum: %d\n", getSum(numbers, i));
+            return 0;
         }
         numbers[i] = atoi(instring);
+
+        if (numbers[i] == 0 && instring[0] != '0'){
+            printf("Warning: Input may not have converted correctly");
+        }
     }
+    printf("Warning: Array size exceeded. Current sum: %i", getSum(numbers, LENGTH));
     return 0;
 }
 
-char* getInput(){
-
-
+char* getInput(char instring[]){
+    fgets(instring, MAXLENGTH, stdin);
+    
+    return instring;
 }
 
 int getSum(int numbers[], int n){
