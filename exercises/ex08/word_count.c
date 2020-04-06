38064b4587f4f7761fbbf46a7161e9a2aeb13353
Colin Snow <colinmsnow@gmail.com>
2020-04-06 19:32:09 -0400

    Word_count working ok

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -3,6 +3,12 @@
 #include <glib.h>
 
 
+void iterator(gpointer key, gpointer value, gpointer user_data) {
+//  printf(user_data, *(gint*)key, value);
+printf("%s = %d\n", key, value);
+}
+
+
 int main(int argc, char **argv){
 
     // FILE* file = g_fopen(argv[1], "w");
@@ -15,13 +21,34 @@ int main(int argc, char **argv){
                      NULL,
                      error);
 
+
     gchar** list = NULL;
-    list = g_strsplit(contents, " ", 2);
+    contents = g_ascii_strdown(contents, -1); // makes everything lower case
+    list = g_strsplit_set(contents, " .", 0); // Splits words and removes periods
 
-    printf("%s", contents);
-    printf("%s", list[0]);
+    int current_value;
 
-    // GHashTable∗ hash = g_hash_table_new(g_str_hash, g_str_equal);
+    GHashTable* dict = g_hash_table_new(g_str_hash, g_str_equal);
+    char *current_word;
+
+    int i = 0;
+    while (1){
+        i +=1 ;
+        if (list[i] == NULL){break;}
+        current_word = list[i];
+         if(g_hash_table_contains(dict, current_word)){
+             current_value = g_hash_table_lookup(dict, current_word);
+             current_value += 1;
+             g_hash_table_replace(dict, current_word, current_value);
+
+         }
+         else{
+            g_hash_table_insert(dict, current_word, 1);
+        }
+    }
+
+    g_hash_table_foreach(dict, (GHFunc)iterator, "%s is %d\n");
+    
 
 
 }
